d99c30a481c7718303d58be4c13eb1109b732311
Vukasin Micic <vukasin.micic5@gmail.com>
2020-12-02 19:47:48 +0100

    Refactor methods

--- main.py
+++ main.py
@@ -29,42 +29,23 @@ def update_requests(restaurant_name, menu_items):
     new_content = ','.join(menu_items.pop(0)) + "\n"
 
     for menu_item_row in menu_items:
+        new_row_content, row_input_valid = preprocess_input_row(menu_item_row)
+        new_content += new_row_content
 
-        if len(menu_item_row) < 3:
-            continue
-        menu_item_name = menu_item_row[0]
-        menu_item_description = menu_item_row[1]
-        menu_item_price = menu_item_row[2]
+        if row_input_valid:
+            menu_item = {'name': menu_item_row[0], 'description': menu_item_row[1],
+                         'price': menu_item_row[2], 'restaurantName': restaurant_name}
 
-        new_content += menu_item_name + ",\"" + menu_item_description + "\"," + menu_item_price
+            response = send_create_menu_item_request(menu_item)
 
-        if menu_item_name == "":
-            new_content += ", You must specify menu item name \n"
-            continue
-
-        if menu_item_price == "":
-            new_content += ", You must specify menu item price \n"
-            continue
-
-        if not is_decimal(menu_item_price):
-            new_content += ", Price must be a decimal number \n"
-            continue
-
-        menu_item = {'name': menu_item_row[0], 'description': menu_item_row[1],
-                     'price': menu_item_row[2], 'restaurantName': restaurant_name}
-
-        create_menu_item_url = "https://voltaire-api-gateway-cvy8ozaz.ew.gateway.dev/menu-items"
-        headers = {'Content-type': 'application/json'}
-        response = requests.post(url=create_menu_item_url, data=json.dumps(menu_item), headers=headers)
-
-        if response.status_code == 201:
-            new_content += ",successful"
-        elif response.status_code == 404:
-            new_content += ",Restaurant name doesn't exist\n"
-            new_content += rewrite_remaining_text(menu_items[1:])
-            break
-        else:
-            new_content += ", Unexpected error"
+            if response.status_code == 201:
+                new_content += ",successful"
+            elif response.status_code == 404:
+                new_content += ",Restaurant name doesn't exist\n"
+                new_content += rewrite_remaining_text(menu_items[1:])
+                break
+            else:
+                new_content += ", Unexpected error"
 
         new_content += "\n"
 
@@ -75,28 +56,36 @@ def update_requests(restaurant_name, menu_items):
         dbx.files_upload(f.read(), "/restaurants/" + restaurant_name + ".csv", mode=WriteMode.overwrite)
 
 
-# def preprocess_row(menu_item_row):
-#     new_row_content = ""
-#     if len(menu_item_row) < 3:
-#         return
-#
-#     menu_item_name = menu_item_row[0]
-#     menu_item_description = menu_item_row[1]
-#     menu_item_price = menu_item_row[2]
-#
-#     new_row_content += menu_item_name + ",\"" + menu_item_description + "\"," + menu_item_price
-#
-#     if menu_item_name == "":
-#         new_row_content += ", You must specify menu item name \n"
-#         return new_row_content
-#
-#     if menu_item_price == "":
-#         new_row_content += ", You must specify menu item price \n"
-#         return new_row_content
-#
-#     if not is_decimal(menu_item_price):
-#         new_row_content += ", Price must be a decimal number \n"
-#         return new_row_content
+def preprocess_input_row(menu_item_row):
+    new_row_content = ""
+    if len(menu_item_row) < 3:
+        return
+
+    menu_item_name = menu_item_row[0]
+    menu_item_description = menu_item_row[1]
+    menu_item_price = menu_item_row[2]
+
+    new_row_content += menu_item_name + ",\"" + menu_item_description + "\"," + menu_item_price
+
+    if menu_item_name == "":
+        new_row_content += ", You must specify menu item name"
+        return new_row_content, False
+
+    if menu_item_price == "":
+        new_row_content += ", You must specify menu item price"
+        return new_row_content, False
+
+    if not is_decimal(menu_item_price):
+        new_row_content += ", Price must be a decimal number"
+        return new_row_content, False
+
+    return new_row_content, True
+
+
+def send_create_menu_item_request(menu_item):
+    create_menu_item_url = "https://voltaire-api-gateway-cvy8ozaz.ew.gateway.dev/menu-items"
+    headers = {'Content-type': 'application/json'}
+    return requests.post(url=create_menu_item_url, data=json.dumps(menu_item), headers=headers)
 
 
 def is_decimal(s):
@@ -117,6 +106,7 @@ def rewrite_remaining_text(menu_items_remaining):
         content += menu_item_name + ",\"" + menu_item_description + "\"," + menu_item_price + "\n"
 
     return content
+
 
 update_menu_items(None)
 
